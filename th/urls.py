c67bd9e46b5732671fd8af4598f7219389c4b8e7
tibyte <ttl74283@gmail.com>
2016-11-13 19:23:45 +0900

    blueprint

---
+++ th/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+	url(r'^$', views.log_list, name='log_list'),
+	url(r'^register/', views.register, name='register'),
+]
---
+++ th/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .models import Log
+from django.http import HttpResponse
+
+# Create your views here.
+def log_list(request):
+	logs = Log.objects.filter().order_by('time')
+	return render(request, 'th/log_list.html', {'logs':logs})
+
+def register(request):
+	time = request.GET.get('time')
+	temp = request.GET.get('temp')
+	humi = request.GET.get('humi')
+	Log.objects.create(
+			time=time, temp=int(temp), humi=int(humi))
+	return HttpResponse("Registerd "+time)
+
