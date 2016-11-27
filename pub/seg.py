7ba09ee81f30514afd26cbe5e99a21d289b1b2bd
tibyte <ttl74283@gmail.com>
2016-11-27 11:34:01 +0000

    Make FND display 2 digits

---
+++ pub/seg.py
@@ -0,0 +1,78 @@
+import RPi.GPIO as GPIO
+import time
+import threading
+
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+
+GPIO.setup(5, GPIO.OUT) #2
+GPIO.setup(6, GPIO.OUT) #5
+GPIO.setup(13, GPIO.OUT) #1
+GPIO.setup(19, GPIO.OUT) #3
+GPIO.setup(26, GPIO.OUT) #4
+GPIO.setup(12, GPIO.OUT) #6
+GPIO.setup(16, GPIO.OUT) #9
+GPIO.setup(20, GPIO.OUT) #7
+GPIO.setup(21, GPIO.OUT) #8
+
+map = {}
+map[1] = 13
+map[2] = 5
+map[3] = 19
+map[4] = 26
+map[5] = 6
+map[6] = 12
+map[7] = 20
+map[8] = 21
+map[9] = 16
+
+seg = [[1,4,3,8,7,6],[3,8],[4,3,9,6,7],
+	[4,3,9,8,7],[1,9,3,8],[4,1,9,8,7],
+	[4,1,9,8,7,6], [1,4,3,8],
+	[4,1,3,9,6,7,8],[1,4,3,9,8,7]]
+
+
+
+def printNum(digit, num):
+
+	for i in range(1, 10):
+		if(i == 2 and i == 5):
+			GPIO.output(map[i], False)
+		else:
+			GPIO.output(map[i], True)
+
+
+	if(digit == 0):
+		GPIO.output(map[2], True)
+		GPIO.output(map[5], False)
+	else:
+		GPIO.output(map[2], False)
+		GPIO.output(map[5], True)
+
+	for i in range(len(seg[num])):
+		GPIO.output(map[seg[num][i]], False)
+	
+num = 0
+tFlag = 1
+
+def handler():
+	while tFlag:
+		printNum(0, num/10%10)
+		time.sleep(1)
+		printNum(1, num%10)
+		time.sleep(1)
+
+th = threading.Thread(target=handler)
+th.start()
+
+
+
+try:
+	while True:
+		num = input()
+except KeyboardInterrupt:
+	tFlag = 0
+	th.join()
+	GPIO.cleanup()
+
+	
